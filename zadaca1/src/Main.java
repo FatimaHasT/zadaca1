43c80adfe0c7ebac8f18658b2df0fbe679def863
Fatima Hasanovic <fatimahasanovic1994@gmail.com>
2023-03-19 18:34:30 +0100

    zadaca1

--- zadaca1/src/Main.java
+++ zadaca1/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Zadatak1");
         /*
          Probajte da dodjelite jednu varijablu jednog tipa u drugu varijablu drugog tipa i zakljucite koje kombinacije se ne mogu dodjeljivati jedne u druge npr :
         String someText = "Some random text"
@@ -16,11 +16,13 @@ public class Main {
         int someNum = 9.0; double ne moze u int
         char firstLetter = "F"; string ne moze u char
 
-
         System.out.println(someText);
         System.out.println(someNumber);
         System.out.println(someNum); */
+        System.out.println("Ne vidi se jer je zakomentarisan");
+        System.out.println();
 
+        System.out.println("Zadatak2");
         // Deklarisati varijable za: Ime i prezime, prvo slovo imena, godina rodjenja, broj 100 000 000 000, broj 42.001
         //imena moraju biti smislena i na engleskom.
 
@@ -31,31 +33,32 @@ public class Main {
         long largeNum = 100_000_000_000L;
         double someNum = 42.001;
 
-        // Isprintate vase ime koristeci samo brojeve  koji se pretvore u char-ove.
-
-        char first = 70;
-        char second = 97;
-        char third = 116;
-        char fourth = 105;
-        char fifth = 109;
-        char sixth = 97;
-
-
-
-
         System.out.println(firstName);
         System.out.println(lastName);
         System.out.println(firstLetter);
         System.out.println(birthDate);
         System.out.println(largeNum);
         System.out.println (someNum);
-        System.out.print (first);
-        System.out.print (second);
-        System.out.print (third);
-        System.out.print (fourth);
-        System.out.print (fifth);
-        System.out.println (sixth);
+        System.out.println();
 
+        System.out.println("Zadatak3");
+        // Isprintate vase ime koristeci samo brojeve  koji se pretvore u char-ove.
+
+        char letterF = 70;
+        char letterA = 97;
+        char letterT = 116;
+        char latterI = 105;
+        char letterM = 109;
+
+        System.out.print (letterF);
+        System.out.print (letterA);
+        System.out.print (letterT);
+        System.out.print (latterI);
+        System.out.print (letterM);
+        System.out.println (letterA);
+        System.out.println();
+
+        System.out.println("Zadatak4");
         /*
          napravite vaijablu koju kad ispisete ispise tekst:
         Answer to the Ultimate Question of Life, the Universe, and Everything is the number 42: true
@@ -68,7 +71,9 @@ public class Main {
 
         System.out.print(someText);
         System.out.println(someBoolean);
+        System.out.println();
 
+        System.out.println("Zadatak5 + Bonus");
         /*
          Zelim da mi napisete program koji ce poceti sa tekstom:
         This is a number xxx and it is about to double;
@@ -85,27 +90,35 @@ public class Main {
         int x = 2;
         int y=x;
         System.out.println("This is a number " + x + " and it is about to double");
+        System.out.println();
         x = x*2;
         System.out.println("This is a number " + x + " and it is about to double");
         System.out.println("The original number has been doubled: " + (x/y) + " times.");
+        System.out.println();
         x = x*2;
         System.out.println("This is a number " + x + " and it is about to double");
         System.out.println("The original number has been doubled: " + (x/y) + " times.");
+        System.out.println();
         x = x*2;
         System.out.println("This is a number " + x + " and it is about to double");
         System.out.println("The original number has been doubled: " + (x/y) + " times.");
+        System.out.println();
         x = x*2;
         System.out.println("This is a number " + x + " and it is about to double");
         System.out.println("The original number has been doubled: " + (x/y) + " times.");
+        System.out.println();
         x = x*2;
         System.out.println("This is a number " + x + " and it is about to double");
         System.out.println("The original number has been doubled: " + (x/y) + " times.");
+        System.out.println();
         x = x*2;
         System.out.println("This is a number " + x + " and it is about to double");
         System.out.println("The original number has been doubled: " + (x/y) + " times.");
+        System.out.println();
         x = x*2;
         System.out.println("This is a number " + x + " and it is about to double");
         System.out.println("The original number has been doubled: " + (x/y) + " times.");
+        System.out.println();
         x = x*2;
         System.out.println("This is a number " + x + " and it is about to double");
         System.out.println("The original number has been doubled: " + (x/y) + " times.");
